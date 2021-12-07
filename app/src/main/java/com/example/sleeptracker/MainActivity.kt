06b319c4d9045670243544fd555b4a1d2832a696
marvel-natanael <63591046+marvel-natanael@users.noreply.github.com>
2021-12-08 02:49:20 +0700

    added screen detection

--- app/src/main/java/com/example/sleeptracker/MainActivity.kt
+++ app/src/main/java/com/example/sleeptracker/MainActivity.kt
@@ -1,42 +1,77 @@
 package com.example.sleeptracker
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.Chronometer
-import android.widget.ImageButton
-import android.widget.Toast
+import android.os.Handler
+import android.view.Menu
+import android.view.MenuItem
 import androidx.lifecycle.ViewModelProvider
 import com.example.sleeptracker.databinding.ActivityMainBinding
+import android.content.IntentFilter
+import android.os.SystemClock
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
+    private val mReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent) {
+            val action = intent.action
+            if (Intent.ACTION_SCREEN_ON == action) {
+                binding.sleepTimeTextView.text = "You have slept for"
+                Handler().postDelayed(this::add, 3000)
+            } else if (Intent.ACTION_SCREEN_OFF == action) {
+                viewModel.startTimer(binding.cMeter)
+            }
+        }
+        fun add() {
+            viewModel.pauseTimer(binding.cMeter)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val meter = binding.cMeter
-        val btn = binding.alarmButton
-        var isWorking = false
+        val intentFilter = IntentFilter(Intent.ACTION_SCREEN_ON)
+        intentFilter.addAction(Intent.ACTION_SCREEN_OFF)
+        registerReceiver(mReceiver, intentFilter)
 
-        btn.setOnClickListener {
-            isWorking = if (!isWorking) {
-                meter.start()
-                true
-            } else {
-                meter.stop()
-                false
-            }
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
 
-            Toast.makeText(this@MainActivity, getString(
-                if (isWorking)
-                    R.string.working
-                else
-                    R.string.stopped),
-                Toast.LENGTH_SHORT).show()
+        binding.alarmButton.setOnClickListener {
+            binding.sleepTimeTextView.text = "Turn off screen to start"
+            binding.cMeter.base = SystemClock.elapsedRealtime()
         }
+        viewModel.currentTimeString.observe(this) {
+            binding.clockTextView.text = it
+        }
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_settings, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                val settingIntent = Intent(this, SettingsActivity::class.java)
+                startActivity(settingIntent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(mReceiver)
     }
 }
--- app/src/main/java/com/example/sleeptracker/MainActivityViewModel.kt
+++ app/src/main/java/com/example/sleeptracker/MainActivityViewModel.kt
@@ -1,47 +1,43 @@
 package com.example.sleeptracker
 
+import android.os.SystemClock
 import android.text.format.DateUtils
+import android.widget.Chronometer
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
-import java.util.*
 
 class MainActivityViewModel : ViewModel() {
-    private var timer = Timer()
-
-    private val initialTime = MutableLiveData<Long>()
+    private var isWorking = false
     private val currentTime = MutableLiveData<Long>()
+    private var chronometerPausedTime: Long = 0
 
-    val currentTimeString = Transformations.map(currentTime) { time ->
+    val currentTimeString: LiveData<String> = Transformations.map(currentTime) { time ->
         DateUtils.formatElapsedTime(time / 1000)
     }
 
-    private val _eventCountDownFinish = MutableLiveData<Boolean>()
-    val eventCountDownFinish: LiveData<Boolean> = _eventCountDownFinish
-
-    private inner class TimeTask(private var time: Long) : TimerTask()
-    {
-        override fun run() {
-            time++
-            currentTime.value = time
+    fun startTimer(meter: Chronometer) {
+        isWorking = if (!isWorking) {
+            meter.base = SystemClock.elapsedRealtime()
+            meter.start()
+            meter.setOnChronometerTickListener {
+                val timeElapsed: Long = SystemClock.elapsedRealtime() - meter.base
+                currentTime.value = timeElapsed
+            }
+            true
+        } else {
+            pauseTimer(meter)
+            false
         }
     }
 
-    fun resetTimer() {
-        timer.cancel()
-        currentTime.value = initialTime.value
-        _eventCountDownFinish.value = true
+    fun pauseTimer(meter: Chronometer) {
+        chronometerPausedTime = SystemClock.elapsedRealtime() - meter.base
+        meter.stop()
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        timer.cancel()
-    }
-
-    companion object
-    {
-        const val TIMER_UPDATED = "timerUpdated"
-        const val TIME_EXTRA = "timeExtra"
+    fun resetTimer(meter: Chronometer){
+        meter.base = SystemClock.elapsedRealtime()
     }
 }
